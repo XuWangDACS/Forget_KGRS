54d894417f44e02336a34bc2857b7c9249896cc9
Xu Wang <xu.wang@maastrichtuniversity.nl>
2024-01-29 15:47:00 +0100

    update

--- extract_rules_from_paths.py
+++ extract_rules_from_paths.py
@@ -9,7 +9,7 @@ relation_domain_range_dict = {
     'directed_by': ('movie', 'director'),
     'edited_by': ('movie', 'editor'),
     'cinematography': ('movie', 'cinematographer'),
-    'wrote_by': ('movie', 'writer'),
+    'wrote_by': ('movie', 'writter'),
     'belong_to': ('movie', 'category'),
     'watched': ('user', 'movie'),
     'produced_by_company': ('producer', 'company')
@@ -27,30 +27,23 @@ def extract_from_line(line):
     for i in range(len(matches) - 1):
         entity1, relation = matches[i]
         entity2, _ = matches[i + 1]
-        if relation:  # Check if there's a relation captured
-            # relations_set.add(relation.strip())  # Add the relation to the set of relations
+        if relation:
             domain_type = relation_domain_range_dict[relation.strip()][0]
             range_type = relation_domain_range_dict[relation.strip()][1]
             if entity1.startswith(domain_type) and entity2.startswith(range_type):
-                # print(f"{entity1}\t{relation.strip()}\t{entity2}")
-                subject = entity1.split(" ")[1]
-                object = entity2.split(" ")[1]
+                subject = entity1.split(" ")[0]+entity1.split(" ")[1]
+                object = entity2.split(" ")[0]+entity2.split(" ")[1]
                 triples.append(f"{relation.strip()}({subject},{object})")
             else:
-                subject = entity2.split(" ")[1]
-                object = entity1.split(" ")[1]
-                # print(f"{entity2}\t{relation.strip()}\t{entity1}")
+                subject = entity2.split(" ")[0]+entity2.split(" ")[1]
+                object = entity1.split(" ")[0]+entity1.split(" ")[1]
                 triples.append(f"{relation.strip()}({subject},{object})")
-            # print(f"{entity1} {relation.strip()} {entity2}")
     return triples
 
 # Process each line separately
 for index, row in file.iterrows():
     path = row['path']
     body = extract_from_line(path)
-    rule = f"recommend({row['uid']},{row['rec item']}) <= {' & '.join(body)}"  # Join the body with spaces
-    print(path)
-    print(rule)
-    print("---- Next Line ----")  # Separator for clarity
-
-# print(relations_set)
+    rule = f"recommend(user{row['uid']},movie{row['rec item']}) <= {' & '.join(body)}"  # Join the body with spaces
+    with open("rules.txt", "a+") as f:
+        f.write(rule + "\n")
